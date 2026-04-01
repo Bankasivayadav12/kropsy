df4e2d23ef582d4af6318867eac56cdd90d5c857
Siva <bankasiva2002@gmail.com>
2026-04-01 19:17:46 +0530

    s

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn-icons-png.flaticon.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn-icons-png.flaticon.com",
+      },
+    ],
   },
 };
+
+export default nextConfig;
 
 module.exports = nextConfig;
